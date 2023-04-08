208c227be0f34367c9c9a1b97991a8a01e50a661
Benoit <benoit.gerin@uclouvain.be>
2023-04-09 01:59:58 +0200

    for 10 compacts

--- Baseline/process.py
+++ Baseline/process.py
@@ -117,7 +117,7 @@ class Baseline(SegmentationAlgorithm):
         self.method = 'logprob_mean_vote_std'
         self.alpha = 1
         self.th = 0.7
-        self.temperature = 15
+        self.temperature = 30
 
 
     def process_case(self, *, idx, case):
